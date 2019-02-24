c82b5c802df34cadfd5bbeb09e2d9231ceaec777
EdWard <ezeward4@gmail.com>
2019-02-24 03:58:48 -0500

    Its Faster

--- sensor/sense/main.cpp
+++ sensor/sense/main.cpp
@@ -30,7 +30,7 @@ auto benchmark(F&& f)
 }
 
 std::atomic<bool> running{true};
-void term(int)
+void term(int i = 0)
 {
 	running = false;
 }
@@ -39,13 +39,15 @@ int main(int argc, char* argv[])
 {
 	using namespace std::chrono_literals;
 	
-	struct sigaction action;
-	memset(&action, 0, sizeof(struct sigaction));
-	action.sa_handler = term;
-	sigaction(SIGTERM, &action, NULL);
-	sigaction(SIGABRT, &action, NULL);
+	{
+		sigset_t set;
+		sigemptyset(&set);
+		sigaddset(&set, SIGTERM);
+		sigaddset(&set, SIGABRT);
+		pthread_sigmask(SIG_BLOCK, &set, NULL);
+	}
 	
-	constexpr auto chunk_size = 256;
+	constexpr auto chunk_size = 64;
 	constexpr auto chunks_per_sample = 4;
 	constexpr auto sample_size = chunk_size * chunks_per_sample;
 	constexpr auto sample_period = 50us;
@@ -53,12 +55,16 @@ int main(int argc, char* argv[])
 	
 	using sample_chunk_t = std::array<hardware::sample_t, chunk_size>;
 	
-	constexpr auto publish_frequency = 50;
+	constexpr auto publish_frequency = 30;
 	constexpr auto publish_period = 1000000us / publish_frequency;
 	
 	std::cout << "Hello, world! (sample frequency = " << sample_frequency << "Hz)\n";
 	
 	hardware h{18};
+	
+	// Set signals after pigpio tries overwriting them
+	signal(SIGTERM, term);
+	signal(SIGABRT, term);
 	
 	sample_chunk_t init{0};
 	// God forbid we're ever behind by 100.
@@ -91,7 +97,20 @@ int main(int argc, char* argv[])
 	// Start Sampling
 	std::thread sampling_thread([&]()
 	{
-		std::cout << "Beginning Sample" << std::endl;
+		std::cout << "Beginning Sample Thread" << std::endl;
+		
+		{
+			cpu_set_t cpu_set;
+			CPU_ZERO(&cpu_set);
+			CPU_SET(0, &cpu_set);
+			if(pthread_setaffinity_np(
+				pthread_self(), sizeof(cpu_set_t), &cpu_set))
+			{
+				term();
+				return;
+			}
+		}
+		
 		while(running)
 		{
 			sample_chunk_t& next = *chunk_q.end();
@@ -103,20 +122,29 @@ int main(int argc, char* argv[])
 		}
 	});
 	
+	
+	{
+		sigset_t set;
+		sigemptyset(&set);
+		sigaddset(&set, SIGTERM);
+		sigaddset(&set, SIGABRT);
+		pthread_sigmask(SIG_UNBLOCK, &set, NULL);
+	}
+	
 	fft_plan fft(sample_size);
 	
 	std::cout << "Connecting to broker..." << std::endl;
 	publisher mqtt("mosquitto");
 	std::cout << "Starting to publish every " << publish_period.count() << std::endl;
 	
-	auto t = clk::now();
-	
 	// Stores the previous values we care about in a queue.
 	circular_queue<short> sample_q(sample_size+1, 0);
 	
 	// Wait for enough samples
-	while(chunk_q.size() < chunk_size && running)
-		std::this_thread::sleep_for(chunk_size*sample_period);
+	while(chunk_q.size() < chunks_per_sample && running)
+		std::this_thread::sleep_for(sample_size*sample_period);
+	
+	auto t = clk::now();
 	
 	while(running)
 	{
@@ -128,7 +156,7 @@ int main(int argc, char* argv[])
 				if(samp == 0)
 					sample_q.push_back(sample_q.back());  // interpolating
 				else
-					sample_q.push_back(sample);
+					sample_q.push_back(samp);
 			}
 			
 			// reset the chunk
